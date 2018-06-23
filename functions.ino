257b2eb1977ff204e50641892295316e3056bf88
teknoa <2teknoa@gmail.com>
2018-06-23 20:21:21 +1000

    Added manual PWM code next to using analogWrite Added fade to color to make smooth transitions.

--- functions.ino
+++ functions.ino
@@ -1,57 +1,147 @@
-void funcSetColor(byte startparamidx) {
-  Serial.println("Executing setcolor");
-  // our parameters start after fnID
-  byte paramidx = startparamidx;
-  
-  byte rgb[] = {0,0,0};
-  byte colidx = 2;
-  byte factor = 1;
-  char curc;
+/**
+ * This file contains all functions avalable to be called over serial.
+ * 
+ * Each function has a init function which is called once before the actual function
+ * is executed.
+ * The function is called every time in the loop as long as the 'runstate' is 
+ * set to R_RUNNING.
+ * If a function is finished, it must set the 'runstate' to R_FINISHED
+ * 
+ * Functions must be non blocking and not using the 'delay' function.
+ * To make your function run "slower" uee the wait variable and count it
+ * until your waited long enough.
+ */
+byte rgb[3] = {0, 0, 0};
+byte oldrgb[3] = {0, 0, 0};
+byte stepping[3];
+int wait;
 
-  // fast forward to end of command
-  while(commandbuffer[paramidx] != '.' && paramidx++ < MAXBUF);
-  paramidx--;
-  Serial.print("found end of command ");
-  Serial.print(paramidx);
-  while(colidx >= 0 && paramidx >= startparamidx - 1) {
-    curc = commandbuffer[paramidx--];
-    Serial.print(curc);
-    if(curc == ','){
-      Serial.println(rgb[colidx]);
-      colidx--;
-      factor = 1;
-      
-    } else {
-      byte c = (byte)curc - 48; // get actual byte value from ascii cipher
-      rgb[colidx] += factor * c;
-      factor *= 10;
-      if(factor > 1000) {
-        Serial.print("Number exceeding 255..invalid");
-        break;
-      }
+#define DELAY 300
+
+void funcSetColorInit(byte startparamidx){
+  //debugprintln("funcSetColorInit");
+  for(byte i = 0; i < 3; i++) {
+    oldrgb[i] = rgb[i];
+  }
+  if(parseParameters(startparamidx, rgb, 3) == -1) {
+    runstate = R_NONE;
+  } else {
+    runstate = R_RUNNING;
+    debugprint("stepping ");
+    for(byte i = 0; i < 3; i++) {
+      if(rgb[i] > oldrgb[i])
+        stepping[i] = 1;
+      else if(rgb[i] < oldrgb[i])
+        stepping[i] = -1;
+      else
+        stepping[i] = 0;
+      debugprint(stepping[i]);
+      debugprint(",");
+    }
+    wait = 0;
+    debugprintln();
+  }
+}
+
+/*
+ * is called, if init function executed without error
+ */
+void funcSetColor(){
+  //debugprintln("writing pins");
+  //debugprint(rgb[0]);
+  //debugprint(",");
+  //debugprint(rgb[1]);
+  //debugprint(",");
+  //debugprint(rgb[2]);
+  
+  if(wait++ >= DELAY) {
+    //debugprintln("wait done.. doing pixel");
+    wait = 0;
+    byte same = 0;
+    
+    for(byte i = 0; i < 3; i++) {
+      if(oldrgb[i] != rgb[i])
+        oldrgb[i] += stepping[i];
+      else
+        same++;
+    } 
+   // debugprint(oldrgb[0]);debugprint(",");
+    //debugprint(oldrgb[1]);debugprint(",");
+    //debugprintln(oldrgb[2]);
+    analogWrite(RPIN, oldrgb[0]);
+    analogWrite(GPIN, oldrgb[1]);
+    analogWrite(BPIN, oldrgb[2]);
+    if(same == 3) {
+      runstate = R_FINISHED;
+     // debugprintln("finished");
     }
   }
   
-  if(colidx == 255) { // after decrement from 0, byte is 255
-    
-    Serial.println("writing pins");
-    Serial.print(rgb[0]);
-    Serial.print(",");
-    Serial.print(rgb[1]);
-    Serial.print(",");
-    Serial.print(rgb[2]);
-    
-    analogWrite(RPIN, rgb[0]);
-    analogWrite(GPIN, rgb[1]);
-    analogWrite(BPIN, rgb[2]);
-  } else {
-    Serial.print("Color missing.");
-  }
-  runstate = R_FINISHED;
-  Serial.println("finished");
 }
 
-void funcFade(byte startparamidx) {
+byte lenR;
+byte lenG;
+byte lenB;
+bool switchR;
+bool switchG;
+bool switchB;
+byte stateR;
+byte stateG;
+byte stateB;
+byte idx;
+
+void funcPWMInit(byte startparamidx) {
+  funcSetColorInit(startparamidx);
+  lenR = rgb[0];
+  lenG = rgb[1];
+  lenB = rgb[2];
+  idx = 0;
+  switchR = switchG = switchB = false;
+  stateR = stateG = stateB = 1;
+  //debugprintln("PWMinit writing pins");
+  //debugprint(rgb[0]);
+  //debugprint(",");
+  //debugprint(rgb[1]);
+  //debugprint(",");
+  //debugprint(rgb[2]);
+
+}
+void funcPWM(){
+  if(idx >= lenR && stateR == 1)
+    switchR = true;
+  if(idx < lenR && stateR == 0)
+    switchR = true;
+    
+  if(switchR) {
+    stateR = 1 - stateR;
+    digitalWrite(RPIN, stateR);
+    switchR = false;
+  }
+
+  if(idx >= lenG && stateG == 1)
+    switchG = true;
+  if(idx < lenG && stateG == 0)
+    switchG = true;
+    
+  if(switchG) {
+    stateG = 1 - stateG;
+    digitalWrite(GPIN, stateG);
+    switchG = false;
+  }
+
+  if(idx >= lenB && stateB == 1)
+    switchB = true;
+  if(idx < lenB && stateB == 0)
+    switchB = true;
+    
+  if(switchB) {
+    stateB = 1 - stateB;
+    digitalWrite(BPIN, stateB);
+    switchB = false;
+  }
+
+  
+  idx++; // it's a byte, so overflow will be to zero
   
 }
 
--- rgbled_command.ino
+++ rgbled_command.ino
@@ -1,13 +1,34 @@
+/**
+ * RGB LED Command
+ * A small program to control RGB led using PWM pins on the arduino
+ * or a manual PWM using digital pins.
+ * The command is received over serial (either USB or bluetooth)
+ * Command form is
+ * CM<fnID>,pval1,pval2,..,pvaln.
+ * 
+ * A command has to start with the chars 'CM'
+ * Followed by a function id, which is the function index
+ * Followed by a list of parameters for the function.
+ * 
+ * Two functions have been implemented for diplaying RGB values using Leds.
+ * See functions file.
+ */
+
 #define MAXBUF 20
 
-#define RPIN 3
-#define GPIN 5
-#define BPIN 6
+#define RPIN 10
+#define GPIN 9
+#define BPIN 8
 
 char* commandbuffer;
 byte cmdbufIdx;
 char valSerial;
+char fnidx;
 
+/*
+ * The command state shows the state, of reading 
+ * a command over serial
+ */
 enum COMMANDSTATE {
   NONE,
   BEGINREAD,
@@ -16,6 +37,9 @@ enum COMMANDSTATE {
 };
 COMMANDSTATE state;
 
+/*
+ * Represents the running state of a function
+ */
 enum RUNSTATE {
   R_NONE,
   R_RUNNING,
@@ -23,25 +47,47 @@ enum RUNSTATE {
 };
 RUNSTATE runstate;
 
+/*
+ * DEBUG block with custom functions
+ * to turn on serial output when debugging but not in production
+ */
+#define DEBUG false
+#define SERIALPORT  Serial1
+
+void debugprintln() {
+  if(DEBUG) SERIALPORT.println();
+}
+void debugprint(byte text) {
+  if(DEBUG) SERIALPORT.print(text);
+}
+void debugprintln(byte text) {
+  if(DEBUG) SERIALPORT.println(text);
+}
+void debugprint(const char text[]) {
+  if(DEBUG) SERIALPORT.print(text);
+}
+void debugprintln(const char text[]) {
+  if(DEBUG) SERIALPORT.println(text);
+}
 
 void readCommand() {
-   valSerial = Serial.read();
+  valSerial = SERIALPORT.read();
   if(valSerial != -1) {
     *(commandbuffer + cmdbufIdx) = valSerial;
     cmdbufIdx++;
     if(state == NONE) {
       if(*(commandbuffer) != 'C') {
         cmdbufIdx = 0;
-        Serial.println("expected 'C' as start of command found.. resetting");
+        debugprintln("expected 'C' as start of command found.. resetting");
       }
       // check, if we have a valid command start
       if(cmdbufIdx == 2) {
         if(*(commandbuffer + 1) == 'M') {
            state = BEGINREAD;
-           Serial.println("start of command found");
+           debugprintln("start of command found");
         } else {
           cmdbufIdx = 0;
-          Serial.println("expected 'CM' as start of command found.. resetting");
+          debugprintln("expected 'CM' as start of command found.. resetting");
         }
       }
     }
@@ -50,36 +96,44 @@ void readCommand() {
       // check, if we have a valid command start
       if(state == BEGINREAD) {
         state = ENDREAD;
-        Serial.println("end of command found");
+        debugprintln("end of command found");
       } else {
         cmdbufIdx = 0;
         state = NONE;
-        Serial.println("endcommand byte found without start command.. resetting");
+        debugprintln("endcommand byte found without start command.. resetting");
       }
     }
     // if sender is still sending command bytes.. reset 
     if(cmdbufIdx >= MAXBUF) {
       state = NONE;
       cmdbufIdx = 0;
-      Serial.println("command too long.. resetting");
+      debugprintln("command too long.. resetting");
     }
   }
   
   if(state == ENDREAD) {
      cmdbufIdx = 0;
+     fnidx = (char)commandbuffer[2];
      state = EXECUTE; 
   }
 
   if(state == EXECUTE) {
     // stop previous running command in case it's an animation
     // run the command
-    if(runstate == R_NONE) {
-      Serial.println("running command");
-      // third position in command buffer is function index starting from 1
-      runstate = R_RUNNING;
-      
-     
+    runstate = R_NONE;
+    debugprintln("running command init");
+    // third position in command buffer is function index starting from 1
+    
+    // call the init part of the function
+    switch(fnidx) {
+      case '1':
+        funcSetColorInit(4);   // param = start of parameters in command buffer
+        break;
+      case '2':
+        funcPWMInit(4);   // param = start of parameters in command buffer
+        break;
     }
+    
     
     
     state = NONE;
@@ -88,18 +142,70 @@ void readCommand() {
   
 }
 
-void parseCommand() {
-  Serial.println("parsing command");
-  
+/*
+ * Reads parameters from command buffer and stores them into given paramarray
+ * It tries to read numparams and not more than numparams.
+ * It will fail, if it cannot read numparams.
+ */
+char parseParameters(byte startparamidx,  byte paramarray[], byte numparams) {
+  debugprintln("parseParameters");
+  // our parameters start after fnID
+  byte paramidx = startparamidx;
+
+  //byte rgb[] = {0, 0, 0};
+  byte colidx = numparams - 1;
+  byte factor = 1;
+  char curc;
+
+  // reset paramarray
+  for(byte i = 0; i< numparams; i++)
+     paramarray[i] = 0;
+
+  // fast forward to end of command
+  while (commandbuffer[paramidx] != '.' && paramidx++ < MAXBUF);
+  paramidx--;
+  debugprint("found end of command at idx");
+  debugprintln(paramidx);
+  while (colidx >= 0 && paramidx >= startparamidx - 1) {
+    curc = commandbuffer[paramidx--];
+    debugprint(curc);
+    if (curc == ',') {
+      debugprintln();
+      debugprint(paramarray[colidx]);
+      colidx--;
+      factor = 1;
+
+    } else {
+      byte c = (byte)curc - 48; // get actual byte value from ascii cipher
+      paramarray[colidx] += factor * c;
+      factor *= 10;
+      if (factor > 1000) {
+        debugprint("Number exceeding 255..invalid");
+        break;
+      }
+    }
+  }
+  if (colidx == 255) { // after decrement from 0, byte is 255
+    for(byte i = 0; i< numparams; i++)
+      debugprint(paramarray[i]);
+      debugprintln("parseParameters complete");
+    return 0; //OK
+  } else {
+     debugprintln("parseParameters incomplete");
+    return -1;
+  }
 }
 
+/*
+ * Arduino setup routine
+ */
 void setup() {
   // put your setup code here, to run once:
   commandbuffer = (char*)malloc(MAXBUF * sizeof(char));
   cmdbufIdx = 0;
   state = NONE;
   runstate = R_NONE;
-  Serial.begin(9600);
+  SERIALPORT.begin(9600);
 
   pinMode(RPIN, OUTPUT);
   pinMode(GPIN, OUTPUT);
@@ -109,12 +215,16 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   readCommand();
+  
   if(runstate == R_RUNNING){
-     switch((char)commandbuffer[2]) {
+     switch(fnidx) {
         case '1':
-          funcSetColor(4);   // param = start of parameters
+          funcSetColor();   // param = start of parameters
+          break;
+        case '2':
+          funcPWM();   // param = start of parameters
           break;
       }
       
